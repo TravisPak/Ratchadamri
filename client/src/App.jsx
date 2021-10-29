6d4ead0a7df9d160f1b2166db61fd53066df9e8d
dmcin003 <dmcin003@fiu.edu>
2021-10-29 00:40:12 -0400

    added axios call for testing server routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ProductDetail from './ProductDetail/index.jsx';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
   super(props);
   this.state = {
-    currentProduct: '';
+    currentProduct: ''
   }
   //BIND FUNCTIONS IN HERE
   }
@@ -14,8 +15,16 @@ class App extends React.Component {
   componentDidMount() {
     this.setState({
       // can randomize product ID later?
-      currentProduct: '37311';
+      currentProduct: '37311'
     })
+    //for testing purposes
+    axios.get('/reviews/meta',{params:{product_id:'37311'}})
+      .then((data)=>{
+        console.log(data);
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
   }
 
 render () {
