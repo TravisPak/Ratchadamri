9492791c46dc2fba0d81e5ebf22e110dc806fc17
dmcin003 <dmcin003@fiu.edu>
2021-10-29 19:39:45 -0400

    added axios request for related items

--- client/src/Cards/index.jsx
+++ client/src/Cards/index.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 
 class Cards extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = {
+      product_id: '37311'
+    };
   }
-  // componentDidMount () {
-  //   axios.get('/qa/questions',{params:{product_id:37311,count:1}}
-  //     .then((result) => console.log('result', result))
-  //     .catch((err) => console.log('error', err));
-  // }
+
+
+  componentDidMount () {
+
+      axios.get(`/products/${this.state.product_id}/related`)
+        .then((res) => {
+          for (var i = 0; i < res.data.length; i++) {
+            axios.get(`products/${res.data[i]}`)
+              .then((result) => console.log(result))
+              .catch((err)=>{console.log(err)});
+          }
+          console.log(res.data);
+        })
+        .catch((err) => console.log(err));
+
+
+  }
 
   render() {
     return (
