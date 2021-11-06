7b96edb3cafc7b94744a3dbae0d36eb59792ea07
Stephen McDaniel <vsmcdaniel2@gmail.com>
2021-11-06 13:44:05 -0500

    functioning modal, no "your outfit cards yet", modal content needs to be added

--- client/src/Cards/index.jsx
+++ client/src/Cards/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import cardHelpers from "./helpers.js";
+import Modal from "./Modal.jsx";
 const findReviewAverage = cardHelpers.cardHelpers.findReviewAverage;
 console.log("findReviewAverage", findReviewAverage);
 
@@ -10,8 +11,13 @@ class Cards extends React.Component {
     this.state = {
       product_id: "37311",
       product_info: {},
+      clickedProduct: '',
+      clickedProductInfo: [],
       relatedInfo: {},
+      modalShowing: false,
     };
+    this.showModal = this.showModal.bind(this);
+    this.hideModal = this.hideModal.bind(this);
   }
 
   componentDidMount() {
@@ -60,15 +66,19 @@ class Cards extends React.Component {
 
       })
       .catch((err) => console.log("Error after loop", err))
-      // .then(
-      //   // console.log('relatedData at end', relatedData);
-      // this.setState({ averages: relatedData })
-      // );
-    //iterate through related
-    //request rating
-    //add to related for each item
     axios.get(`products/${this.state.product_id}`)
       .then((result) => {this.setState({product_info: result.data})})
+  }
+
+  //SHOW MODAL
+  showModal(e){
+    console.log(e.name);
+    this.setState({modalShowing:true, clickedProductInfo: e.features, clickedProduct: e.name});
+  }
+
+  //HIDE MODAL
+  hideModal(){
+    this.setState({modalShowing:false});
   }
 
   render() {
@@ -84,13 +94,39 @@ class Cards extends React.Component {
               <span className="item-category item-text" >{this.state.relatedInfo[key][0].category}</span>
               <br />
               <span className="item-name item-text">{this.state.relatedInfo[key][0].name}</span>
-              {/* <br /> */}
               <span className="item-price item-text">{'$' + this.state.relatedInfo[key][0].default_price}</span>
               <br />
               <span className="stars item-text">{'Stars: ' + this.state.relatedInfo[key][1]}</span>
+              <a onClick={() => {this.showModal(this.state.relatedInfo[key][0])}}>⭐</a>
             </div>
           ))
         }
+        <Modal isShowing={this.state.modalShowing} handleClose={this.hideModal}><div>
+          <table className="related-modal">
+            <tr>
+              <th>{this.state.clickedProduct}</th>
+              <th>Characteristics</th>
+              <th>{this.state.product_info.name}</th>
+            </tr>
+            {/* iterate on characteristics of clicked and add to table
+              then iterate on characteristics of main and add to table */
+              this.state.clickedProductInfo.map((feature) => {
+                return (<tr>
+                  <td>Placeholder</td>
+                  <td>{feature.feature}</td>
+                  <td>Placeholder</td>
+                </tr>)
+              })
+              // this.state.product_info.features.map((feature) => {
+              //   return (<tr>
+              //     <td>Placeholder</td>
+              //     <td>{feature.feature}</td>
+              //     <td>Placeholder</td>
+              //   </tr>)
+              // })
+              }
+          </table>
+          </div></Modal>
       </div>
     );
   }
