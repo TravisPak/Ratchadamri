3bc1defd01fdd78547b27fc15ae2291fd043d3aa
dmcin003 <dmcin003@fiu.edu>
2021-11-06 15:10:40 -0400

    added Photo upload form

---
+++ client/src/Reviews&Ratings/PhotosForm.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+class PhotosForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { image: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    // setPhotos in here
+    this.props.addPhoto(this.state.image);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          value={this.state.image}
+          name="image"
+          onChange={this.handleChange}
+        ></input>
+        <input type="submit" value="Upload" />
+      </form>
+    );
+  }
+}
+
+export default PhotosForm;
