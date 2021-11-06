bdd57d51f9d1e87193b584922ac48afb8344b6cf
Stephen McDaniel <vsmcdaniel2@gmail.com>
2021-11-06 13:53:57 -0500

    removed console logs for merge

--- client/src/Cards/index.jsx
+++ client/src/Cards/index.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import cardHelpers from "./helpers.js";
 import Modal from "./Modal.jsx";
 const findReviewAverage = cardHelpers.cardHelpers.findReviewAverage;
-console.log("findReviewAverage", findReviewAverage);
+
 
 class Cards extends React.Component {
   constructor(props) {
@@ -72,7 +72,7 @@ class Cards extends React.Component {
 
   //SHOW MODAL
   showModal(e){
-    console.log(e.name);
+    // console.log(e.name);
     this.setState({modalShowing:true, clickedProductInfo: e.features, clickedProduct: e.name});
   }
 
@@ -103,11 +103,9 @@ class Cards extends React.Component {
         }
         <Modal isShowing={this.state.modalShowing} handleClose={this.hideModal}><div>
           <table className="related-modal">
-            <tr>
-              <th>{this.state.clickedProduct}</th>
-              <th>Characteristics</th>
-              <th>{this.state.product_info.name}</th>
-            </tr>
+              <tr>{this.state.clickedProduct}</tr>
+              <tr>Characteristics</tr>
+              <tr>{this.state.product_info.name}</tr>
             {/* iterate on characteristics of clicked and add to table
               then iterate on characteristics of main and add to table */
               this.state.clickedProductInfo.map((feature) => {
